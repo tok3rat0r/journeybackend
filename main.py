d7d0e86125c382025671a79e7d8a182f20372ea7
Anzigar <ansigare@gmail.com>
2023-01-24 16:54:01 +0300

    ----database

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from item import StreaksSchema, UserSchema, WorkoutSchema, PaymetSchema, Subscri
 from flask_restful import Resource
 from flask_bcrypt import bcrypt
 import re
-from datetime import datetime, timedelta
+from datetime import datetime
 from app import app
 
 
