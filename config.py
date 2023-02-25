37c0d055d7f1a5ea6ca95bb1c2198fcff0cbcf8e
tok3rat0r <owenmjones1@gmail.com>
2023-02-25 14:27:27 +0530

    Undo changes to database URI and requirements

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/journey'
 # SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:8889/journey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECURITY_PASSWORD_HASH = 'bcrypt'
